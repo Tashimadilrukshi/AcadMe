02138db546e6fcdcbbb0dee254ee242d94518333
Misco <kmtsandeepanie@gmail.com>
2021-01-28 09:11:00 +0530

    update file structure

--- src/layouts/defaultLayout/defaultLayout.js
+++ src/layouts/defaultLayout/defaultLayout.js
@@ -1,17 +1,19 @@
 import React from "react";
-import Nav from "../components/nav";
+import Nav from "../../components/nav";
+import './defaultLayout.css';
+
 
 const LandingLayout = (ViewComponent) => {
     return class extends React.Component {
         render() {
             return (
-                <>
+                <div className="background">
                     <Nav/>
                     <div>
                         <ViewComponent />
                     </div>
                     {/* Add Footer and any other components to the default layout */}
-                </>
+                </div>
             );
         }
     };
